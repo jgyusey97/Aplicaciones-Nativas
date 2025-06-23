0fa87861e3b8ef4d4851d2f0685892faf95f1f74
jgyusey97 <61711332+jgyusey97@users.noreply.github.com>
2025-06-23 17:47:39 -0500

    Primera definición de arquitectura de libreria de Android utilizando kotlin

---
+++ AppModuleActivacionTarjetasAndroid/BancoGuayaquilActivacionTarjeta/src/main/java/com/bg/bancoguayaquilactivaciontarjeta/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.bg.bancoguayaquilactivaciontarjeta.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val backgroundColor = Color(0xFFFFFFFF)
+val primaryColor = Color(0xFFD2006E)
---
+++ AppModuleActivacionTarjetasAndroid/BancoGuayaquilActivacionTarjeta/src/main/java/com/bg/bancoguayaquilactivaciontarjeta/ui/theme/Type.kt.kt
@@ -0,0 +1,15 @@
+package com.bg.bancoguayaquilactivaciontarjeta.ui.theme
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    )
+)
